7805ffa4f218579f012305c23983fd79adabb7b7
ericrav <ericrabinowitz@me.com>
2014-02-17 19:03:23 -0500

    fix conflicts

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
-<<<<<<< HEAD
- var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-48127888-1']);
-  _gaq.push(['_trackPageview']);
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-48127888-1']);
+_gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 
-var sys = new PixelSystem(document.getElementById("canvas"), 85, 70, 10);
-=======
 var sys = new PixelSystem(document.getElementById("canvas"), 85, 70, 10, document.getElementById("spectrum1"), document.getElementById("pointer1"));
->>>>>>> master
 
 window.onkeyup = function(e) {
 	var key = e.keyCode ? e.keyCode : e.which;
@@ -34,17 +30,13 @@ window.onkeyup = function(e) {
 		_gaq.push(['_trackEvent', 'Control', 'Toggle Eraser', 'E']);
 	} else if (key == 67) { // c clear drawing
 		sys.clear();
-<<<<<<< HEAD
 		_gaq.push(['_trackEvent', 'Control', 'Clear', 'C']);
 	}
 	else if (key == 65) { // a animate colors
 		sys.toggleAnimate();
 		_gaq.push(['_trackEvent', 'Control', 'Animate', 'A']);
-=======
-	} else if (key == 65) { // a animate colors
-		sys.toggleAnimate();
 	} else if (key == 32) { // SPACE toggle spectrum
 		sys.toggleSpectrum();
->>>>>>> master
+		_gaq.push(['_trackEvent', 'Control', 'Change spectrum', 'SPACE']);
 	}
 }
